b6e79a80c58feddbb0336bb3eb5c4d479701ba85
Matheus Luiz <matheuslfdd096@gmail.com>
2025-10-10 22:02:08 -0300

    Criação dos componentes de layouts de registro e login

---
+++ src/app/components/layouts/register-layout/register-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-layout',
+  imports: [],
+  templateUrl: './register-layout.html',
+  styleUrl: './register-layout.scss'
+})
+export class RegisterLayout {
+
+}
