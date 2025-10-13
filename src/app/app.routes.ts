d87968f14dde9d88d693ab9b177eda5c15f1401f
Matheus Luiz <matheuslfdd096@gmail.com>
2025-10-13 19:46:29 -0300

    Adicionado as rotas de registro e login e ajustado a rota raiz para redirecionar para registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { GameList } from './pages/game-list/game-list';
+import { Login } from './pages/login/login';
+import { AuthGuard } from './auth/auth.guard';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, // redireciona '' para /home acessando a rota raiz do projeto
-  { path: 'home', component: Home },   // página inicial
-  { path: 'games', component: GameList } // lista de jogos
+  { path: 'login', component: Login },         // página de login
+  { path: 'register', component: Register },   // página de registro
+  { path: 'home', component: Home, canActivate: [AuthGuard] }, // home protegida
+  { path: 'games', component: GameList, canActivate: [AuthGuard] }, // lista de jogos protegida
+  { path: '', redirectTo: 'register', pathMatch: 'full' }, // raiz redireciona para register
+  { path: '**', redirectTo: 'register' } // qualquer rota desconhecida redireciona para register
 ];
