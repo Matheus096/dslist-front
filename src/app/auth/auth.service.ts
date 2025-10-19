332c01c0c3000a37a6f061fe0d6e49c64ebc8b5e
Matheus Luiz <matheuslfdd096@gmail.com>
2025-10-19 18:09:15 -0300

    Criação da estrutura de autenticação com JWT, implementação dos services e guards necessários para permitir acesso as rotas apenas se o usuário estiver logado

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl + '/login', { username, password });
+  }
+
+  register(username: string, password: string) {
+    return this.http.post(this.apiUrl + '/register', { username, password }, { responseType: 'text' });
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}
