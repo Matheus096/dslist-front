1272b97ecc24d8fb8ad44b2bd566a12b1d7a979d
Matheus Luiz <matheuslfdd096@gmail.com>
2025-10-22 01:11:22 -0300

    Implementado funcionalidade de deslogar na navbar

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
