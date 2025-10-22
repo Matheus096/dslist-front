e37531f6abaf365190c242cba1f37641dc0c96e6
Matheus Luiz <matheuslfdd096@gmail.com>
2025-10-22 01:13:27 -0300

    Implementado as funcionalidades de login e registro de usuários

--- src/app/components/layouts/login-layout/login-layout.ts
+++ src/app/components/layouts/login-layout/login-layout.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-layout',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login-layout.html',
-  styleUrl: './login-layout.scss'
+  styleUrls: ['./login-layout.scss']
 })
-export class LoginLayout {
+export class LoginComponentLayout {
+  username = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: response => {
+        const token = response.token;
+        this.authService.saveToken(token);
+        this.router.navigate(['/home']);
+      },
+      error: err => {
+        alert('Usuário ou senha inválidos!');
+        console.error(err);
+      }
+    });
+  }
 }
--- src/app/components/layouts/register-layout/register-layout.ts
+++ src/app/components/layouts/register-layout/register-layout.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register-layout',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './register-layout.html',
-  styleUrl: './register-layout.scss'
+  styleUrls: ['./register-layout.scss']
 })
-export class RegisterLayout {
+export class RegisterComponentLayout {
 
+  username: string = '';
+  password: string = '';
+  constructor(private authService: AuthService, private router: Router) {}
+
+  register() {
+    this.authService.register(this.username, this.password).subscribe({
+      next: (res) => {
+        alert(res);
+        this.router.navigate(['/login']); // redireciona para login após registrar
+      },
+      error: (err) => {
+        alert(err.error);
+      }
+    });
+  }
 }
